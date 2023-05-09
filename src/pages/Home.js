dbb531f2e4d7ebb014d1aea79c5af5f18774df75
Jegadeesh2 <sjegadeeshwaran2@gmail.com>
2023-05-09 16:12:31 +0530

    type edit modified

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,39 @@
 import React from "react";
-import {Typewriter} from "react-simple-typewriter"
-import homePic from "../images/homeImage.svg"
+import { Typewriter } from "react-simple-typewriter";
+import homePic from "../images/homeImage.svg";
 
 const Home = () => {
-  return ( 
+  return (
     <div className="home-page">
       <div className="home-content">
-        <p className="greetings">Hi There ! <span className="wave"> 👋 </span></p>
-        <p className="name">I'M <span> JEGADEESHWARAN</span></p>
-        <p className="role">  <Typewriter
-            words={['FULL STACK DEVELOPER', "FRONT END DEVELOPER", 'MERN STACK DEVELOPER']}
+        <p className="greetings">
+          Hi There ! <span className="wave"> 👋 </span>
+        </p>
+        <p className="name">
+          I'M <span> JEGADEESHWARAN</span>
+        </p>
+        <p className="role">
+          {" "}
+          <Typewriter
+            words={[
+              "FRONT END DEVELOPER",
+              "REACT DEVELOPER",
+              "MERN STACK DEVELOPER",
+            ]}
             loop={5}
             cursor
-            cursorStyle='_'
+            cursorStyle="_"
             typeSpeed={150}
             deleteSpeed={40}
             delaySpeed={2000}
-          /></p>
+          />
+        </p>
       </div>
       <div className="home-image">
-        <img src={homePic} alt="Developer-Table" className="home-pic"/>
+        <img src={homePic} alt="Developer-Table" className="home-pic" />
       </div>
     </div>
-  )
+  );
 };
 
 export default Home;
